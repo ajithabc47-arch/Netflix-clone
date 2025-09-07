df346c03e4b8c119d8dadae91b2017d92ff9d7b3
Ajith Kumar <ajithabc47@gmail.com>
2025-09-07 19:25:51 +0530

    Add files via upload

---
+++ profiles.js
@@ -0,0 +1,7 @@
+function selectProfile(name) {
+      alert(`Welcome ${name}!`);
+        window.location.href = "index.html";
+        }
+        function addProfile() {
+          alert("Feature coming soon!");
+          }
